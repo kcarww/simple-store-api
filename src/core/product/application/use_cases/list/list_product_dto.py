4c78cc30d9a97e49d5e43ae7ef74c5306b7b107d
kcarww <carlos.oe.cc@gmail.com>
2024-12-30 15:21:44 -0300

    feat: :sparkles: list products use case
    create use case for list products, pagination and unit test

---
+++ src/core/product/application/use_cases/list/list_product_dto.py
@@ -0,0 +1,39 @@
+from abc import ABC
+from ast import TypeVar
+from dataclasses import dataclass, field
+from typing import Generic, TypeVar
+from uuid import UUID
+
+import config
+
+
+@dataclass
+class ListProductRequest:
+    order_by: str = "name"
+    current_page: int = 1
+
+
+@dataclass
+class ListProductOutput:
+    id: UUID
+    name: str
+    price: float
+    stock: int
+    active: bool
+    created_at: str
+    updated_at: str
+
+@dataclass
+class ListOutputMeta:
+    current_page: int = 1
+    per_page: int = config.DEFAULT_PAGINATION_SIZE
+    total: int = 0
+
+T = TypeVar("T")
+
+@dataclass
+class ListProductResponse(Generic[T], ABC):
+    data: list[T]
+    meta: ListOutputMeta = field(default_factory=ListOutputMeta)
+
+
---
+++ src/core/product/application/use_cases/list/list_product_use_case.py
@@ -0,0 +1,36 @@
+from dataclasses import dataclass
+
+import config
+from core.product.application.use_cases.list.list_product_dto import ListOutputMeta, ListProductOutput, ListProductRequest, ListProductResponse
+from core.product.domain.product_repository import ProductRepositoryInterface
+
+@dataclass
+class ListProductUseCase:
+    product_repository: ProductRepositoryInterface
+
+    def execute(self, request: ListProductRequest) -> ListProductResponse:
+        products = self.product_repository.find_all()
+        sorted_products = sorted(
+            [
+                ListProductOutput(
+                    id=product.id,
+                    name=product.name,
+                    price=product.price,
+                    stock=product.stock,
+                    active=product.active,
+                    created_at=product.created_at,
+                    updated_at=product.updated_at,
+                ) for product in products
+            ], key=lambda product: getattr(product, request.order_by)
+        )
+        page_offset = (request.current_page - 1) * config.DEFAULT_PAGINATION_SIZE
+        products_page = sorted_products[page_offset: page_offset + config.DEFAULT_PAGINATION_SIZE]
+
+        return ListProductResponse(
+            data=products_page,
+            meta=ListOutputMeta(
+                current_page=request.current_page,
+                per_page=config.DEFAULT_PAGINATION_SIZE,
+                total=len(sorted_products)
+            )
+        )
